2929e23b5220f181e433a6cb268d44b4c3fb936e
Muhammad Zaki Jauhari <mzakijauhari@gmail.com>
2026-07-06 19:13:55 +0700

    modifikasi seed admin

--- backend/scripts/seed_admin.py
+++ backend/scripts/seed_admin.py
@@ -9,10 +9,6 @@ import sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from passlib.context import CryptContext
